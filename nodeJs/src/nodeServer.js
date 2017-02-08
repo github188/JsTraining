f4e54eeb29d47b60b3a49963757e24eb8180f720
ztree <hunter.z@263.net>
2017-02-08 16:17:44 +0800

    04 作业

--- nodeJs/src/nodeServer.js
+++ nodeJs/src/nodeServer.js
@@ -2,7 +2,14 @@ const hostname = '127.0.0.1';
 const port = 80;
 
 var express = require('express');
+var proxy = require('http-proxy-middleware');
 var app = express();
+
+
+
+app.use('/api', proxy({ target: 'http://note.wiz.cn', changeOrigin: true }));
+app.use('/wizas', proxy({ target: 'http://note.wiz.cn', changeOrigin: true }));
+
 
 app.use(express.static(__dirname + '/../../page_src'));
 console.log('Web Path: ' + __dirname + '/../../page_src');
--- page_src/04/test.js
+++ page_src/04/test.js
@@ -21,27 +21,86 @@ function initDom() {
             return;
         }
 
-        login(userName, password);
+        login(userName.value, password.value);
 
     });
 }
 
 function login(userName, password) {
     $.ajax({
-		url: "/api/login",
-		type: "get",
-		async: true,
-		data: {
+        url: "/api/login",
+        type: "post",
+        async: true,
+        data: {
             client_type: 'web2.0',
             api_version: 6,
             user_id: userName,
             password: password
         },
-		success: function(res) {
-			console.log(res);
-		},
-		error: function(jqXHR, textStatus, errorThrown) {
-			console.log('login error.');
-		}
-	});
+        success: function (res) {
+            console.log(res);
+        },
+        error: function (jqXHR, textStatus, errorThrown) {
+            console.log('login error.');
+        }
+    });
+}
+
+function testInterval() {
+    var counter = 0;
+    var interval = setInterval(function () {
+        console.log(new Date().valueOf());
+        counter++;
+        if (counter > 100) {
+            console.log('clear Interval');
+            clearInterval(interval);
+        }
+
+        (function () {
+            var i, j, k;
+            for (i = 0; i < 99999; i++) {
+                for (j = 0; j < 39999; j++) {
+                    k = i * j;
+                }
+            }
+        })()
+        console.log(new Date().valueOf() + '  --  interval ' + counter + ' over.')
+
+    }, 2000);
+}
+
+
+
+var TIMER = null;
+function setTimer(callback, sleep) {
+    if (!!TIMER && !callback) {
+        clearTimeout(TIMER);
+    }
+    TIMER = setTimeout(function() {
+        callback();
+        setTimer(callback, sleep);
+    }, sleep);
+};
+
+function testTimer() {
+    var counter = 0;
+    setTimer(function () {
+        console.log(new Date().valueOf());
+        counter++;
+        if (counter > 100) {
+            console.log('clear timer');
+            setTimer();
+        }
+
+        (function () {
+            var i, j, k;
+            for (i = 0; i < 99999; i++) {
+                for (j = 0; j < 39999; j++) {
+                    k = i * j;
+                }
+            }
+        })()
+        console.log(new Date().valueOf() + '  --  timer ' + counter + ' over.')
+
+    }, 2000);
 }
