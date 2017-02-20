b54f7d100af2d6df4814c30301cf630687395ed8
ztree <hunter.z@263.net>
2017-02-20 09:46:05 +0800

    nodeJs 01 作业

--- nodeJs/src/testReadFile/readSync.js
+++ nodeJs/src/testReadFile/readSync.js
@@ -2,24 +2,26 @@ const fs = require('fs');
 const StartFile = 'files/start.txt';
 
 
-let readFile = (filePath, callback) => {
-    let data;
+let filePath = StartFile;
+let data;
+do {    
     try {
         data = fs.readFileSync(filePath, 'utf8');
     } catch(e) {
         throw e;
     }
-    callback(data);
-}
 
-let onRead = (data) => {
-    let txt = data.trim();
-    console.log(txt);
-    if (txt == 'done') {
+    if (!data) {
         process.exit();
         return;
     }
-    readFile('files/' + txt + '.txt', onRead);
-}
 
-readFile(StartFile, onRead);
+    data = data.trim();
+    console.log(data);
+    if (data == 'done') {
+        process.exit();
+        return;
+    }
+    filePath = 'files/' + data + '.txt';
+} while (data);
+
